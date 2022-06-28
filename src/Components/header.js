cd0f41c3461abbff2661ac192a85288e1d4aa592
saniaayoub <saniaayoub555@gmail.com>
2022-06-28 18:01:39 +0500

    second day

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,13 +1,15 @@
+import { Button } from 'bootstrap'
 import React from 'react'
 import logo from '../images/logo.png'
 import './header.css'
+import img from '../images/img.svg'
 const Header = () => {
   return (
     <>
       <header>
         <div className="container">
           <div className="row text-center">
-            <div className="col-md-3">
+            <div className="col-md-4">
               <div className="logo_div">
                 <img
                   src={logo}
@@ -18,14 +20,37 @@ const Header = () => {
                 />
               </div>
             </div>
-            <div className="col-md-6"></div>
-            <div className="col-md-3"></div>
+            <div className="col-md-4">
+              <nav className="nav-class">
+                <div className="nav-div">
+                  <ul className="nav-list-custom">
+                    <li className="nav-item">
+                      <a className="nav-tag" href="#">
+                        Anywhere
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-tag" href="#">
+                        Any week
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-tag" href="#">
+                        Add guests
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+            <div className="col-md-4"></div>
           </div>
           <div className="row text-center">
             <div className="col-md-12">
               <p> hello</p>
             </div>
           </div>
+          <div></div>
         </div>
       </header>
     </>
